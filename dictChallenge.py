6e2fcc805feb73dd3a23ea3bc42f2360787f44fb
Divya Dinesh Menon <139251706+Divyadmenon098@users.noreply.github.com>
2025-02-16 02:39:15 +0000

    Update dictChallenge.py

--- dictChallenge.py
+++ dictChallenge.py
@@ -1,4 +1,18 @@
+#Tried using defaultdict:
+"""Defaultdict automatically sets missing word counts to 0, so we dont have to check if a word exists before incrementing. Comparing the two codes the one with 
+defaultdict takes less execution time but slightly takes more memory space as will automatically create a new entry and initialize the value to the default i.e. 0 here.""" 
+
+from collections import defaultdict
 def count_words(input_str):
+    word_count = defaultdict(int)
+    for word in input_str.lower().split():
+        word_count[word] += 1
+    return dict(word_count)
+input_string = "This is a sample string. This string is just an example."
+print(count_words(input_string))
+
+#Source code
+"""def count_words(input_str):
     word_count = {}
     words = input_str.lower().split()
 
@@ -13,4 +27,6 @@ def count_words(input_str):
 
 input_string = "This is a sample string. This string is just an example."
 word_counts = count_words(input_string)
-print(word_counts)
+print(word_counts)"""
+
+
